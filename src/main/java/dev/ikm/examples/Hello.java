e2d6a97a5c5888d0428ca826647c261fceddbabe
Jason Taylor <jason@jasongtaylor.com>
2024-05-20 13:25:23 -0600

    Adding updates from Jason

--- src/main/java/dev/ikm/examples/Hello.java
+++ src/main/java/dev/ikm/examples/Hello.java
@@ -2,6 +2,6 @@ package dev.ikm.examples;
 
 class Hello {
     public static void main(String []args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, Awesome World!");
     }
 }
