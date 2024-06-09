89a029e0b18eb732e11d92312a21a32ac4e45aa1
Cas Aras <arascas@yahoo.com>
2024-06-08 20:25:11 -0400

    new changes

--- src/main/java/dev/ikm/examples/Hello.java
+++ src/main/java/dev/ikm/examples/Hello.java
@@ -4,4 +4,20 @@ class Hello {
     public static void main(String []args) {
         System.out.println("Hello, Super Awesome World!");
     }
+    
+    public int sum(int x, int y) {
+    	return x+y;
+    }
+
+    public int multiply(int x, int y) {
+    	return x*y;
+    }
+    
+    public int substract(int x, int y) {
+    	return x-y;
+    }
+    
+    public float divide(int x, int y) {
+    	return x/y;
+    }    
 }
