3a44c69e73902769165f082b49371b98b591b3c3
Cas Aras <arascas@yahoo.com>
2024-07-22 12:42:26 -0400

    test files added

--- src/main/java/dev/ikm/examples/Hello.java
+++ src/main/java/dev/ikm/examples/Hello.java
@@ -1,7 +1,28 @@
 package dev.ikm.examples;
 
+
 class Hello {
     public static void main(String []args) {
         System.out.println("Hello, Super Awesome World!");
     }
+    
+    public int sum(int x, int y) {
+    	return x+y;
+    }
+
+    public int multiply(int x, int y) {
+    	return x*y;
+    }
+    
+    public int substract(int x, int y) {
+    	return x-y;
+    }
+    
+    public float divide(int x, int y) {
+    	return x/y;
+    }   
+
+    public float dividebytwo(int x, int y) {
+    	return (x+y)/2;
+    } 
 }
